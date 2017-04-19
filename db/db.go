6419618cf9ffaad93e510d98c013e6058433c7e9
bodyno <az8321550@gmail.com>
2017-04-19 12:28:36 +0800

    optimize db address

--- db/db.go
+++ db/db.go
@@ -8,7 +8,7 @@ import (
 var instance *gorm.DB
 
 func Init() {
-	db, err := gorm.Open("mysql", "baobaobooks_dev:Y4GPcRojd8wfZzIqMLD7VUle9OS356@(rm-bp1199ayx32ah1u80o.mysql.rds.aliyuncs.com:3306)/baobaobooks?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "root:Xuzeyu123@(rm-wz9643u6cranpzqm9o.mysql.rds.aliyuncs.com:3306)/nobody?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 		panic("falied to connect database")
