68a846594c908dde31d8a0d0d7fe749efad94e28
bodyno <411298027@qq.com>
2017-04-19 17:20:15 +0800

    add config

---
+++ config/config.go
@@ -0,0 +1,5 @@
+package config
+
+const (
+	Mysql = "root:Xuzeyu123@(rm-wz9643u6cranpzqm9o.mysql.rds.aliyuncs.com:3306)/nobody?charset=utf8&parseTime=True&loc=Local"
+)
--- db/db.go
+++ db/db.go
@@ -3,12 +3,13 @@ package db
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"nobody/go-study/config"
 )
 
 var instance *gorm.DB
 
 func Init() {
-	db, err := gorm.Open("mysql", "root:Xuzeyu123@(rm-wz9643u6cranpzqm9o.mysql.rds.aliyuncs.com:3306)/nobody?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", config.Mysql)
 	if err != nil {
 		panic(err)
 		panic("falied to connect database")
