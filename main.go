58a9d3683e6e83f70a5ce4e88a0616abe15b4ff2
bodyno <411298027@qq.com>
2017-05-08 20:26:01 +0800

    add redis and memcache

--- main.go
+++ main.go
@@ -8,36 +8,58 @@ import (
 	"flag"
 	"github.com/bodyno/go-study/config"
 	"github.com/bodyno/go-study/routers"
-	"github.com/go-bongo/bongo"
-	"log"
+	//"github.com/go-bongo/bongo"
+	"github.com/go-redis/redis"
+	"github.com/bradfitz/gomemcache/memcache"
+	//"log"
 	"fmt"
 )
 
 func main() {
 
-	mongoConfig := &bongo.Config{
-		ConnectionString: "mongodb://root:MGhYIjJURXQiS3e@118.178.240.112:3717/admin",
-		Database: "mongo-test",
-	}
-	connection, err := bongo.Connect(mongoConfig)
+	mc := memcache.New("118.178.240.112:11212")
+
+	it, err := mc.Get("foo")
+	fmt.Println(it)
+
+	client := redis.NewClient(&redis.Options{
+		Addr: "118.178.240.112:63791",
+		Password: "o6d8EfxGkF7wAPm",
+		DB: 0,
+	})
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+
+	val, err := client.Get("daixiao:user:77777888").Result()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	fmt.Println("mongodb连接成功")
+	fmt.Println("key", val)
 
-	type Person struct {
-		bongo.DocumentBase `bson:",inline"`
-		FirstName string
-		LastName string
-		Gender string
-	}
 
-	myPerson := &Person{
-		FirstName: "Testy",
-		LastName: "McGee",
-		Gender: "male",
-	}
-	connection.Collection("people").Save(myPerson)
+	//mongoConfig := &bongo.Config{
+	//	ConnectionString: "mongodb://root:MGhYIjJURXQiS3e@118.178.240.112:3717/admin",
+	//	Database: "mongo-test",
+	//}
+	//connection, err := bongo.Connect(mongoConfig)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println("mongodb连接成功")
+	//
+	//type Person struct {
+	//	bongo.DocumentBase `bson:",inline"`
+	//	FirstName string
+	//	LastName string
+	//	Gender string
+	//}
+	//
+	//myPerson := &Person{
+	//	FirstName: "Testy",
+	//	LastName: "McGee",
+	//	Gender: "male",
+	//}
+	//connection.Collection("people").Save(myPerson)
 
 
 
