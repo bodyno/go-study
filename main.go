b03845edf7e9426be8db3f2c1b76df6fd658c49f
bodyno <411298027@qq.com>
2017-05-08 19:39:08 +0800

    optimize

--- main.go
+++ main.go
@@ -8,9 +8,38 @@ import (
 	"flag"
 	"github.com/bodyno/go-study/config"
 	"github.com/bodyno/go-study/routers"
+	"github.com/go-bongo/bongo"
+	"log"
+	"fmt"
 )
 
 func main() {
+
+	mongoConfig := &bongo.Config{
+		ConnectionString: "mongodb://root:MGhYIjJURXQiS3e@118.178.240.112:3717/admin",
+		Database: "mongo-test",
+	}
+	connection, err := bongo.Connect(mongoConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("mongodb连接成功")
+
+	type Person struct {
+		bongo.DocumentBase `bson:",inline"`
+		FirstName string
+		LastName string
+		Gender string
+	}
+
+	myPerson := &Person{
+		FirstName: "Testy",
+		LastName: "McGee",
+		Gender: "male",
+	}
+	connection.Collection("people").Save(myPerson)
+
+
 
 	flag.IntVar(&config.Age, "age", 2, "age of gopher")
 	flag.Parse()
